edf9bd691618014f878f21f8cd4c2da02e4867a8
freddy2022J <freddycyiza3@gmail.com>
2024-12-13 21:45:54 +0200

    vue pro

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Calculator</h1>
+    <input type="number" v-model="number1" />
+    <select v-model="selectedOperation">
+      <option value="addition">+</option>
+      <option value="subtraction">-</option>
+      <option value="multiplication">*</option>
+      <option value="division">/</option>
+    </select>
+    <input type="number" v-model="number2" />
+    <button @click="calculate()">Calculate</button>
+    <h2>Result: {{ result }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number1: null,
+      number2: null,
+      selectedOperation: "addition",
+      result: null
+    };
+  },
+  methods: {
+    calculate() {
+      switch (this.selectedOperation) {
+        case "addition":
+          this.result = this.number1 + this.number2;
+          ;
+        case "subtraction":
+          this.result = this.number1 - this.number2;
+          break;
+        case "multiplication":
+          this.result = this.number1 * this.number2;
+          break;
+        case "division":
+          this.result = this.number1 / this.number2;
+          break;
+        default:
+          this.result = null;
+          break;
+      }
+    }
+  }
+};
+</script>
